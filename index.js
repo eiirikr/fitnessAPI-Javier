1f3584f6cd9d53b1281d4ade92e3690f6ac7b51b
javierjericm <javierjericm@gmail.com>
2025-01-16 22:24:14 +0800

    Updated index.js

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
+// [SECTION] Dependencies and Modules
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/user");
-const workoutRoutes = require("./routes/workout");
+const cors = require("cors");
 
-dotenv.config();
+// [SECTION] Routes
+const userRoutes = require("./routes/user.js");
+const workoutRoutes = require("./routes/workout.js");
+
+// [SECTION] Environment Setup
+require("dotenv").config();
+
+// [SECTION] Server Setup
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// DB connection
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.error(err));
+// Update CORS options to use BASE_URL from the environment
+const corsOptions = {
+  origin: [
+    "http://localhost:8000",
+    "http://localhost:4000",
+    "http://localhost:3000",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-// Routes
+app.use(cors(corsOptions));
+
+// [SECTION] Database Connection
+mongoose.connect(process.env.MONGODB_URI, {});
+mongoose.connection.once("open", () =>
+  console.log("Now Connected to MongoDB Atlas.")
+);
+
+// [SECTION] Backend Routes
 app.use("/users", userRoutes);
 app.use("/workouts", workoutRoutes);
 
+// [SECTION] Server Gateway Response
 if (require.main === module) {
   app.listen(process.env.PORT || 4000, () => {
     console.log(`API is now online on port ${process.env.PORT || 4000}`);
